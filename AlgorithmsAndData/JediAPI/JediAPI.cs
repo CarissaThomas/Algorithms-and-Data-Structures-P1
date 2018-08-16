8d5a3ed498f0c8726ee2de5c3b9cf7e50dd16ff3
RUBY\carissa.thomas <cthomas@callruby.com>
2018-08-16 12:02:21 -0700

    All jedis API call completed

--- AlgorithmsAndData/JediAPI/JediAPI.cs
+++ AlgorithmsAndData/JediAPI/JediAPI.cs
@@ -1,6 +1,9 @@
 ﻿using AlgorithmsAndData.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
@@ -11,6 +14,40 @@ namespace AlgorithmsAndData.JediAPI
     public class JediAPI
     {
       static HttpClient client = new HttpClient();
+
+
+      public static async Task<List<Models.Jedi>> GetAllJedis()//Return all Jedis
+      {
+         try
+         {
+            List<Jedi> jediDeserialized = new List<Jedi>();
+
+            var _Data = new Dictionary<Jedi, JArray>();
+
+            AllJedis jedi = new AllJedis();
+
+            HttpResponseMessage response = await client.GetAsync("https://swapi.co/api/people/");
+
+            if (response.IsSuccessStatusCode)
+            {
+               jedi = await response.Content.ReadAsAsync<AllJedis>();
+
+               string jsonstring = JsonConvert.SerializeObject(jedi.Results);
+               jediDeserialized = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Jedi>>(jsonstring);
+
+            }
+            return jediDeserialized;
+
+         }
+         catch (Exception ex)
+         {
+            Console.WriteLine(ex);
+            throw;
+         }
+
+         
+
+      }
 
       public static async Task<Models.Jedi> GetJedis(double ID)
       {
---
+++ AlgorithmsAndData/Models/AllJedis.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmsAndData.Models
+{
+    class AllJedis
+    {
+      public int Count { get; set; }
+      public List<Jedi> Results { get; set; }
+   }
+}
